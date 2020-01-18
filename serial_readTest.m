516dfb9cebb91d30bf91d4eb338ab77ba36859a4
William Stoy <williamstoy@icloud.com>
2020-01-18 16:54:45 -0500

    added atm switching on ch1 when breakin

--- serial_readTest.m
+++ serial_readTest.m
@@ -1,28 +1,106 @@
 clc; clear; close all;
 
-m = modbus('serialrtu', '/dev/cu.usbserial-FT3NO6ER1', 'BaudRate', 115200, 'Parity', 'even', 'Timeout', .1);
+serverId = 247;
+readAddress = 49;
+writeAddress = 50;
+baudRate = 115200;
+parity = 'even';
+precision = 'uint16';
+
+m1 = proportionAirModbus('serialrtu', '/dev/cu.usbserial-FT3NO6ER0', 'BaudRate', baudRate, 'Parity', parity, 'Timeout', .1);
+m2 = proportionAirModbus('serialrtu', '/dev/cu.usbserial-FT3NO6ER1', 'BaudRate', baudRate, 'Parity', parity, 'Timeout', .1);
+m3 = proportionAirModbus('serialrtu', '/dev/cu.usbserial-FT3NO6ER2', 'BaudRate', baudRate, 'Parity', parity, 'Timeout', .1);
+m4 = proportionAirModbus('serialrtu', '/dev/cu.usbserial-FT3NO6ER3', 'BaudRate', baudRate, 'Parity', parity, 'Timeout', .1);
+
+handles = [m1 m2 m3 m4];
+
+COMMAND = 10000;
 
 figure;
-h = animatedline;
-ax = gca;
-ax.YGrid = 'on';
-ylabel('Pressure (mbar)');
+subplot(4,1,1);
+h_pressure1 = animatedline('Color','m','LineWidth',3);
+h_cmd1 = animatedline('Color','g','LineWidth',5);
+ax1 = gca;
+ax1.YGrid = 'on';
+ylabel('Pressure Value');
+
+subplot(4,1,2);
+h_pressure2 = animatedline('Color','m','LineWidth',3);
+h_cmd2 = animatedline('Color','g','LineWidth',5);
+%h2 = animatedline;
+ax2 = gca;
+ax2.YGrid = 'on';
+ylabel('Pressure Value');
+
+subplot(4,1,3);
+h_pressure3 = animatedline('Color','m','LineWidth',3);
+h_cmd3 = animatedline('Color','g','LineWidth',5);
+%h2 = animatedline;
+ax3 = gca;
+ax3.YGrid = 'on';
+ylabel('Pressure Value');
+
+subplot(4,1,4);
+h_pressure4 = animatedline('Color','m','LineWidth',3);
+h_cmd4 = animatedline('Color','g','LineWidth',5);
+%h2 = animatedline;
+ax4 = gca;
+ax4.YGrid = 'on';
+ylabel('Pressure Value');
 
 startTime = datetime('now');
 
-for i = 1:1000
+prevNow =  datetime('now');
+randWaitTime = 10;
+calibCMD = 0;
+
+write(handles(4),'holdingregs', writeAddress, calibCMD, serverId, precision);
+
+while true
     try
-        data = read(m,'holdingregs', 49, 1, 247, 'uint16');
-        pressure = (data - 27041)/39.21;
-        t =  datetime('now') - startTime;
-        % Add points to animation
-        addpoints(h,datenum(t),pressure)
-        % Update axes
-        ax.XLim = datenum([t-seconds(15) t]);
-        datetick('x','keeplimits')
-        drawnow;
-    
+        now =  datetime('now');
+        %getDataAndPlot(handles(1),ax1, h_pressure1, h_cmd1, startTime);
+        %getDataAndPlot(handles(2),ax2, h_pressure2, h_cmd2, startTime);
+        %getDataAndPlot(handles(3),ax3, h_pressure3, h_cmd3, startTime);
+        getDataAndPlot(handles(4),ax4, h_pressure4, h_cmd4, startTime);
+        
+        if(posixtime(now) - posixtime(prevNow) > 10)
+            write(handles(4),'holdingregs', writeAddress, calibCMD, serverId, precision);
+            ylabel(num2str(calibCMD));
+            prevNow = now;
+            calibCMD = calibCMD + 4000;
+            
+            beep; 
+            
+            if(calibCMD > 65535)
+                break;
+            end
+        end
+        % wait a random amount of time between  pressure changes
+%         if(posixtime(now) - posixtime(prevNow) > randWaitTime)
+%             write(handles(randi([1,4])),'holdingregs', writeAddress, randi([0,65535]), serverId, precision);
+%             prevNow = now;
+%             randWaitTime = randi([5,15]);
+%             disp('changing pressure');
+%         end
     catch error
-       disp(error.identifier);
+       disp(error);
+       break;
     end
+end
+
+
+function getDataAndPlot(m,ax, h_pressure, h_cmd, startTime)
+    t =  datetime('now') - startTime;
+    
+    data = read(m,'holdingregs', 49, 2, 247, 'uint16');
+    pressure = data(1);
+    command = data(2);
+    
+    addpoints(h_pressure,datenum(t),pressure);
+    addpoints(h_cmd,datenum(t),command);
+    
+    ax.XLim = datenum([t-seconds(15) t]);
+    datetick('x','keeplimits')
+    drawnow;
 end
